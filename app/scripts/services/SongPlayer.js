1efcc61ff1bee967faab2fc11d817e583cf3b19d
ethan021 <ethan021@me.com>
2016-12-07 10:12:53 -0800

    Assignment 8 services 3: Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -27,8 +27,7 @@
     */
     var setSong = function(song) {
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong(song);
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -62,6 +61,16 @@
     var playSong = function(song) {
       currentBuzzObject.play();
       song.playing = true;
+    };
+
+    /**
+    * @function stopSong
+    * @desc Stops sound from playing through users speakers and set song.playing to null
+    * @type {Object}
+    **/
+    var stopSong = function(song) {
+      currentBuzzObject.stop();
+      song.playing = null;
     };
 
     /**
@@ -101,13 +110,27 @@
       currentSongIndex--;
 
       if (currentSongIndex < 0) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong(song);
       } else {
         var song = currentAlbum.songs[currentSongIndex];
         setSong(song);
         playSong(song);
       }
+    };
+
+    /**
+    * @function this function goes to the next song
+    * @return none
+    **/
+    SongPlayer.next = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex++;
+      if (currentSongIndex >= currentAlbum.songs.length) {
+          currentSongIndex = 0;
+      }
+      var song = currentAlbum.songs[currentSongIndex];
+      setSong(song);
+      playSong(song);
     };
 
     return SongPlayer;
